e8f591f391d4598b34858f762228b4e3fe0a0c90
mervebilic <merveebilic@icloud.com>
2022-05-04 02:29:37 +0300

    list add

---
+++ src/components/Contacts/index.js
@@ -0,0 +1,24 @@
+import {useEffect, useState} from 'react';
+import "./styles.css";
+
+import List from "./List";
+import Form from "./Form";
+
+function Contacts() {
+const [contacts, setContacts ] = useState([ ]); 
+
+useEffect(() => {
+console.log(contacts);
+}, [contacts]);
+
+
+  return (
+    <div id="container"> 
+    <h1>Contacts</h1>
+    <List contacts={contacts}   />
+    <Form addContact={setContacts}  contacts={contacts}/>
+    </div>
+  );
+}
+
+export default Contacts;
